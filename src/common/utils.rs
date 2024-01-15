2f5d15cdad1bf3105b476f933f05875970745e2d
garkling <gladfering@gmail.com>
2024-01-15 04:58:18 +0200

    Minor fixes

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -2,11 +2,9 @@ use std::env;
 
 use log4rs;
 
-
 pub fn init_logger() {
     let log_path = env::var("HOME").unwrap_or(String::from("."));
     let log_file = format!("{}/log-config.yml", log_path);
 
     log4rs::init_file(log_file, Default::default()).unwrap();
-
 }
